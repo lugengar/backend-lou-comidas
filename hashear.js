1239279f8bbd4642af483f6f4f6d525d4691e377
lugengar <luciano.barbinii@gmail.com>
2025-01-14 01:09:54 -0300

    a

--- hashear.js
+++ hashear.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs');
 
-const password = 'loucomidas';  // La contraseña que deseas guardar
+const password = '';  // La contraseña que deseas guardar
 bcrypt.hash(password, 10).then(hash => {
   console.log(hash);  // Guarda este hash en tu archivo .env
 });
